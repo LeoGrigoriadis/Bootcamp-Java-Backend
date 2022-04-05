7e85fb7fe89b8a631eb14b7606f7b2e69e08149b
LeoGrigoriadis <89147723+LeoGrigoriadis@users.noreply.github.com>
2022-04-05 12:05:30 -0300

    Add files via upload

--- Clase02/src/Arreglos.java
+++ Clase02/src/Arreglos.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Arreglos {
@@ -17,26 +18,27 @@ public class Arreglos {
 		Mostrar(vector);
 	}
 	private void Mostrar(int vector[]) {
-		int mayor=0, menor=10000,pares[], par=0, impares[], impar=0, descendente[];
-		pares= new int[10]; impares=new int[10]; descendente=new int[10];
+		int mayor=0, menor=10000,pares[], par=0, impares[], impar=0, ascendente[], descendente[];
+		pares= new int[10]; impares=new int[10]; descendente=new int[10]; ascendente=new int[10];
 		for(int i=0;i<10;i++) {
 			if(vector[i]>mayor) mayor=vector[i];
 			if(vector[i]<menor) menor=vector[i];
 			if(vector[i]%2==0) {pares[par]=vector[i]; par++;}
 			if(vector[i]%2!=0) {impares[impar]=vector[i]; impar++;}
-			
+			ascendente[i]=vector[i];
 		}
+		Arrays.sort(ascendente);
 		int aux=0;
 		for(int i=9;i>=0;i--) {
-			vector[i]=descendente[aux]; aux++;
+			descendente[i]=ascendente[aux]; aux++;
 		}
 		System.out.println("El mayor es "+mayor+", el menor es "+menor+", los numeros pares son los siguientes: ");
 		Imprimir(pares);
 		System.out.println("Los impares son los siguientes: ");
 		Imprimir(impares);
-		System.out.println("Los ascendentes son: ");
-		Imprimir(vector);
-		System.out.println("Y los descendentes son: ");
+		System.out.println("En orden ascendente es: ");
+		Imprimir(ascendente);
+		System.out.println("En orden descendente es: ");
 		Imprimir(descendente);
 	}
 	private void Imprimir(int vector[]) {
