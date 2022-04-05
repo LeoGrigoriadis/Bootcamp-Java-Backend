9fa6ed3cedf644f1f629e6e89b246dc082739053
LeoGrigoriadis <89147723+LeoGrigoriadis@users.noreply.github.com>
2022-04-05 12:18:02 -0300

    Add files via upload

--- Clase02/src/Minuscula.java
+++ Clase02/src/Minuscula.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
 public class Minuscula {
-	private String cadena;
+	//private String cadena;
 	public Minuscula() {
+		/* Entendí mal la consigna, aca la corrijo.
 		Scanner teclado = new Scanner(System.in);
 		System.out.print("Ingrese una cadena de texto: ");
 		this.cadena=teclado.next();
-		Comprobar(cadena);
+		Comprobar(cadena);*/
+		char letra;
+
+        for(letra = 'a'; letra<='z'; ++letra){
+            System.out.println(letra + "");
+        }
 	}
-	private void Comprobar(String texto) {
+	/*private void Comprobar(String texto) {
 		char vector[], letra;
 		int cont=0;
 		vector= new char[texto.length()];
@@ -21,7 +27,7 @@ public class Minuscula {
 			}
 		}
 		System.out.print(vector);
-	}
+	}*/
 	public static void main(String[] args) {
 		Minuscula m=new Minuscula();
 	}
