2d7f248ba7d7cbb3509c216332d1848118d5be56
LeoGrigoriadis <89147723+LeoGrigoriadis@users.noreply.github.com>
2022-04-06 13:47:58 -0300

    Add files via upload

--- Clase03/src/EjerciciosDía02/Ahorcado.java
+++ Clase03/src/EjerciciosDía02/Ahorcado.java
@@ -3,38 +3,50 @@ import java.util.Vector;
 import java.util.Scanner;
 
 public class Ahorcado {
-	private String palabraj1, palabraj2, pistaj1, pistaj2;
-	private Vector j1=new Vector(2), j2=new Vector(2); //nombre, puntos;
+	private String j1, j2, jactual, palabra, pista;
+	private int p1=0,p2=0, turno=0;
 	Scanner teclado = new Scanner(System.in);
 	
 	public Ahorcado() {
-		System.out.print("Ingrese el nombre del jugador 1: ");
-		j1.addElement(teclado.next());
-		System.out.print("Ingrese el nombre del jugador 2: ");
-		j2.addElement(teclado.next());
-		System.out.print(j1.elementAt(0)+" comienza ingresando una palabra: ");
-		palabraj1=teclado.next();
-		System.out.print(j1.elementAt(0)+" agrega una pista: ");
-		pistaj1=teclado.next();
-
-		j1.addElement(0);
-		j2.addElement(0);
 		
-		div();
-			if(adivinar(palabraj1)) {
-				System.out.print("Ganaste 1 punto "+j2.elementAt(0));
-				j2.insertElementAt(1, 1);
-			}else {
-				System.out.print("Ganaste 1 punto "+j1.elementAt(0));
-				j1.insertElementAt(1, 1);;
-			}
+		System.out.print("Ingrese el nombre del jugador 1: ");
+		j1=teclado.next();
+		System.out.print("Ingrese el nombre del jugador 2: ");
+		j2=teclado.next();
+		jactual=j1;
+			for(int i=0;i<5;i++) {
+				System.out.print(jactual+" comienza ingresando una palabra: ");
+				palabra=teclado.next();
+				System.out.print(jactual+" agrega una pista: ");
+				pista=teclado.next();
+
+				div();
+					if(adivinar(palabra)&&jactual==j1) {
+						p2++;
+						System.out.println("\n¡Ganaste 1 punto "+j2+"!");
+						jactual=j1;
+					}else if(!adivinar(palabra)&&jactual==j1){
+						p1++;
+						System.out.println("\n¡Ganaste 1 punto "+j1+"!");
+						jactual=j1;
+					}else if(!adivinar(palabra)&&jactual==j2){
+						p2++;
+						System.out.println("\n¡Ganaste 1 punto "+j2+"!");
+						jactual=j2;
+					}else if(adivinar(palabra)&&jactual==j2){
+						p1++;
+						System.out.println("\n¡Ganaste 1 punto "+j1+"!");
+						jactual=j1;
+					}
+					System.out.println("El puntaje actual es: "+j1+"= "+p1+" - "+j2+"= "+p2);
+				}
 	}
 	private boolean adivinar(String p1) {
 		char[] caracteres;
 		char letra;
 		char[] letrasAcertadas;
 		boolean acerto=false;
-		int trys=6, cont=0;
+		int trys=6, cont=0, acertadas=0;
 		caracteres=new char[trys];
 		letrasAcertadas=new char[p1.length()];
 		
@@ -44,20 +56,33 @@ public class Ahorcado {
 			
 			if(comprobarRepeticion(letra, caracteres)) {
 				System.out.println("La letra es repetida.");
-			}else if(comprobarAcertadas(letra, p1)) {
-				
-				//me falta la parte donde muestra la palabra con las letras 
-				//que va adivinando y le permite ganar al jugador 2.
+			}else if(comprobarAcertadas(letra, p1, letrasAcertadas)) {
+				if(!comprobarRepeticion(letra, letrasAcertadas)) {
+					letrasAcertadas[acertadas]=letra;
+					acertadas++;
+				}
+				if(acertadas==p1.length()) acerto=true;
 			}
 			else {
 				caracteres[cont]=letra;
 				cont++; trys--;
 			}
-			mostrarIngresadas(caracteres, trys);
+			
+			System.out.println("Letras acertadas: ");
+			mostrarAcertadas(letrasAcertadas);
+			System.out.println("Intentos restantes: "+trys);
+			System.out.println("Letras incorrectas: ");
+			mostrarIngresadas(caracteres);
 		}
 		return acerto;
 	}
-	private boolean comprobarAcertadas(char letra, String p1) {
+	private void mostrarAcertadas(char[] car) {
+		for(int i=0;i<car.length;i++) {
+			System.out.print(car[i]+" - ");
+		}
+		System.out.println("");
+	}
+	private boolean comprobarAcertadas(char letra, String p1, char[]letrasAcertadas) {
 		boolean flag=false;
 		char[] palabra=p1.toCharArray();
 		for(int i=0;i<p1.length();i++) {
@@ -67,8 +92,7 @@ public class Ahorcado {
 		}
 		return flag;
 	}
-	private void mostrarIngresadas(char[] car, int trys) {
-		System.out.println("Intentos restantes: "+trys);
+	private void mostrarIngresadas(char[] car) {
 		for(int i=0;i<car.length;i++) {
 			System.out.print(car[i]+" - ");
 		}
@@ -76,7 +100,7 @@ public class Ahorcado {
 	}
 	private boolean comprobarRepeticion(char letra, char[]caracter) {
 		boolean flag=false;
-		for(int i=0;i<caracter.length-1;i++) {
+		for(int i=0;i<caracter.length;i++) {
 			if(caracter[i]==letra) {
 				flag=true;
 			}
