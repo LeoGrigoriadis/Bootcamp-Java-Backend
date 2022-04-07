17de325fc52ae73279e18f2d3c3debc390280fe5
LeoGrigoriadis <89147723+LeoGrigoriadis@users.noreply.github.com>
2022-04-07 11:26:38 -0300

    Add files via upload

---
+++ Clase04/index.java
@@ -0,0 +1,18 @@
+
+public class index {
+
+	public static void main(String[] args) {
+		Remolque r=new Remolque(20);
+		Particular p=new Particular("MRS432", 4);
+		System.out.print(p.getMatricula()+" "); p.setAcelerar(150);
+		System.out.println(p.toString());
+		Carga c=new Carga("TAS118");
+		System.out.print(c.getMatricula()+" ");c.ponRemolque(r);
+		System.out.print(c.getMatricula()+" ");c.setAcelerar(120);
+		System.out.println(c.toString());
+		System.out.print(c.getMatricula()+" ");c.quitarRemolque();
+		System.out.print(c.getMatricula()+" ");c.setAcelerar(120);
+		System.out.println(c.toString());
+	}
+
+}
