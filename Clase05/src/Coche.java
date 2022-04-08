8d2ea69d5ac155527036d20dc7fe0634ba9b2cf2
LeoGrigoriadis <89147723+LeoGrigoriadis@users.noreply.github.com>
2022-04-08 11:58:05 -0300

    Add files via upload

--- Clase05/src/Coche.java
+++ Clase05/src/Coche.java
@@ -1,12 +1,14 @@
 
 public class Coche implements imprimirDatos{
 	private String matricula, marca, modelo;
-	
+	//constructor
 	public Coche(String matricula, String marca, String modelo) {
 		this.matricula=matricula;
 		this.marca=marca;
 		this.modelo=modelo;
 	}
+	//imprimir datos
+	@Override
 	public void imprimir() {
 		System.out.print("matricula: "+matricula+" - marca: "+marca+" - modelo: "+modelo);
 	}
