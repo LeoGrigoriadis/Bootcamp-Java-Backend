b6970bdeaa1421fd5e7d9463cdc4346a8ea60104
LeoGrigoriadis <89147723+LeoGrigoriadis@users.noreply.github.com>
2022-04-10 17:03:09 -0300

    Add files via upload

---
+++ 0Practicas_individuales_e_independientes/Hospital_db/src/Classes/Empleado.java
@@ -0,0 +1,8 @@
+package Classes;
+
+public class Empleado extends Usuario{
+	//constructor
+	public Empleado(String nombreUser, String contraUser, int id, boolean esCliente) { 
+		super(nombreUser, contraUser, id, esCliente);
+		}
+}
